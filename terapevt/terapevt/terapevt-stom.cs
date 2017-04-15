578717b493d4a905779dc1eb02e80b53cac1a315
OlgaGladkova <gladkovao1994@mail.ru>
2017-04-15 18:17:22 +0700

    Пройден тест Test_get_question_about_pain.
    Signed-off-by: OlgaGladkova <gladkovao1994@mail.ru>

--- terapevt/terapevt/terapevt-stom.cs
+++ terapevt/terapevt/terapevt-stom.cs
@@ -5,34 +5,44 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-    public class terapevt_stom
+public class terapevt_stom
+{
+    string[] question, answer, diagnosis;
+
+    public terapevt_stom()
     {
-        string[] question, answer, diagnosis;
+        question = new string[] 
+        { "Здравствуйте!",
+          "Что Вас беспокоит? Зубная боль?",
+          "Боль какая? Острая, тупая, приступообразная или постоянная?" };
+        answer = new string[] { "Здравствуйте", "Здравствуйте!" };
+        diagnosis = new string[1];
+    }
 
-        public terapevt_stom()
+    public string get_question(string ans)
+    {
+        string ret_question;
+        switch (ans)
         {
-            question = new string[] { "Здравствуйте!", "Что Вас беспокоит? Зубная боль?" };
-            answer = new string[] { "Здравствуйте", "Здравствуйте!"};
-            diagnosis = new string[1];
-        }
-
-        public string get_question(string ans)
-        {
-            string ret_question;
-            switch(ans)
-            {
             case "Здравствуйте": ret_question = question[1]; break;
+            case "Да": ret_question = question[2]; break;
+            case "нет": ret_question = "Я вряд ли Вам помогу, обратитесь к другому врачу)"; break;
             default:
                 ret_question = "Я Вас не понимаю!"; break;
-            }
-            return ret_question;
+        }
+        return ret_question;
+    }
+
+    public void visit_a_doctor()
+    {
+        Console.WriteLine(question[0]);
+        string ans = Console.ReadLine();
+        while(ans!="")
+        {
+            Console.WriteLine(get_question(ans));
+            ans = Console.ReadLine();
         }
 
-        public void visit_a_doctor()
-        {
-            Console.WriteLine(question[0]);
-            Console.WriteLine(get_question(Console.ReadLine()));
-            Console.ReadLine();
     }
 
 }
