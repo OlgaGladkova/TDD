4132dcd3de4693fc01bc71e58ceef33975cedc8b
OlgaGladkova <gladkovao1994@mail.ru>
2017-04-16 02:09:25 +0700

    Пройден тест Test_get_question_about_pain_while_eating_sweet
    Signed-off-by: OlgaGladkova <gladkovao1994@mail.ru>

--- terapevt/terapevt/terapevt-stom.cs
+++ terapevt/terapevt/terapevt-stom.cs
@@ -25,7 +25,8 @@ public class terapevt_stom
             "обострение хронического периодонтита.",
             "периодонтит.",
             "пульпит.",
-            "глубокий кариес."
+            "глубокий кариес.",
+            "средний кариес."
         };
     }
 
@@ -47,6 +48,7 @@ public class terapevt_stom
             case "Каждый прием":
             case "Каждый прием пищи": 
             case "При каждом прием пищи": ret_question = answer[0] + diagnosis[4] + answer[1]; break;
+            case "При употреблении сладкого": ret_question = answer[0] + diagnosis[5] + answer[1]; break;
             default:
                 ret_question = "Я Вас не понимаю!"; break;
         }
